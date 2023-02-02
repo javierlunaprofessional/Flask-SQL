ba36bcdc61d4b2fb4cb58d38e3ebdc43e9c07a59
javiluna84 <javier.luna.jrlf@gmail.com>
2023-02-02 15:26:57 -0500

    done

--- app-with-mysql.py
+++ app-with-mysql.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'write your database endpoint'
+app.config['MYSQL_DATABASE_HOST'] = 'database-1.cs40sa4fagy7.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
